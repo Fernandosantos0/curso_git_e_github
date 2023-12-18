b4acb639b0121718ccc2770af31ba3bb078a514c
Fernando Santos <fernandomeca7@gmail.com>
2023-12-18 15:30:04 -0300

    Imprementando o sistema de login de conta

---
+++ login.js
@@ -0,0 +1,14 @@
+/* Funçãopara logar no sistema */
+function login(nomeConta, senha) {
+    /* Toda a lógica para logar no sistema */
+}
+
+const nome = 'Fernando Santos Pereira';
+
+console.log(nome.slice(0, nome.length));
+
+String.prototype.reverseText = function() {
+    return this.split('').reverse().join('');
+};
+
+console.log('Meu nome é Fernando Santos!'.reverseText());
